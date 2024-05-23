73dbfa13c5e93d4a128ce26342b9287306abfd73
Kwadwo Owusu <mr_kwadwo.owusu@yahoo.com>
2024-05-23 01:55:02 -0400

    FEATURE: add employee repository

--- src/main/java/io/trackIt/assetManager/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package io.trackIt.assetManager;
-
-import io.trackIt.assetManager.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByAssetsIsNull();
-}
